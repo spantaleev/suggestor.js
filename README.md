9ca6f0a96c98899b44f3aee141a245dea8d64826
Slavi Pantaleev <s.pantaleev@gmail.com>
2013-11-09 09:22:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,420 @@
+suggestor.js
+============
+
+**suggestor.js** is a suggestions-providing / @user mentions / #tagging / autocompletion library.
+
+It's similar to [Mention.js](https://github.com/jakiestfu/Mention.js), but does NOT depend on [Twitter Bootstrap and Typeahead](https://github.com/twitter/bootstrap).
+It's also less hacky, more flexible and supposedly much less buggy.
+
+
+Dependencies
+------------
+
+ * [jQuery](http://jquery.com/)
+
+
+Features
+--------
+
+ * Twitter Bootstrap look, without depending on it
+ * Configurable start delimiter (**@query**, **#query**, etc.)
+ * Predefined HTML templates for easily implementing @user mentions and #tagging
+ * Flexible suggestions data sources (local, lazily-loaded from remote, per-query-loaded from remote, etc.)
+ * Matching the query against more than 1 keyword (user mentions can use username, first name, last name, etc.)
+ * Very customizable - various parts can be replaced with your own logic
+
+
+Intro
+-----
+
+Suggestions are provided by a local or remote (lazily-loaded) data set.
+A data set is an array of objects, which looks like this:
+
+`````javascript
+[
+	{"id": "github"},
+	{"id": "gmail"},
+	{"id": "facebook"},
+	{"id": "twitter"}
+]
+`````
+
+Each entry **must** contain an **id** field at least.
+The **id** is the value that gets used when an entry is selected from the suggestions list.
+
+Queries are matched against each entry's `id` in the data set.
+A query for "#g" will match both "#github" and "#gmail".
+
+A **keywords** field can be specified to allow matching against more values, not just **id**.
+
+`````javascript
+[
+	{
+		"id": "john.doe",
+		"keywords": ["John", "Doe"]
+	},
+	{
+		"id": "john.brown",
+		"keywords": ["John", "Brown"]
+	}
+]
+`````
+
+
+Usage
+-----
+
+Initialization:
+
+`````html
+<script type="text/javascript" src="jquery.js"></script>
+
+<script type="text/javascript" src="suggestor.js"></script>
+<link rel="stylesheet" href="suggestor.css" />
+`````
+
+
+### Example 1
+
+Simple tag suggestions, triggered by a #hash.
+The predefined `tags` suggestions template is used, which influences how the suggestions list looks like.
+
+`````javascript
+(function () {
+	var dataSet = [
+		{"id": "github"},
+		{"id": "gmail"},
+		{"id": "facebook"},
+		{"id": "twitter"}
+	];
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplate": "tags",
+		"dataSource": new Suggestor.LocalDataSource(dataSet)
+	});
+})();
+`````
+
+
+### Example 2
+
+In this case the query is matched against the id field and all other keywords.
+"#social" would trigger suggestions for "#github", "#facebook" and "#twitter".
+
+`````javascript
+(function () {
+	var dataSet = [
+		{"id": "github", "keywords": ["github", "social", "version-control"]},
+		{"id": "gmail", "keywords": ["gmail", "google", "mail"]},
+		{"id": "facebook", "keywords": ["facebook", "social"]},
+		{"id": "twitter", "keywords": ["twitter", "social"]}
+	];
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplate": "tags",
+		"dataSource": new Suggestor.LocalDataSource(dataSet)
+	});
+})();
+`````
+
+
+### Example 3
+
+In this case the number of suggestions is limited to 2.
+"#social" would trigger suggestions for "#github" and "#facebook" ONLY, not for "#twitter".
+
+`````javascript
+(function () {
+	var dataSet = [
+		{"id": "github", "keywords": ["github", "social", "version-control"]},
+		{"id": "gmail", "keywords": ["gmail", "google", "mail"]},
+		{"id": "facebook", "keywords": ["facebook", "social"]},
+		{"id": "twitter", "keywords": ["twitter", "social"]}
+	];
+
+	var suggestionsLimit = 2;
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplate": "tags",
+		"dataSource": new Suggestor.LocalDataSource(dataSet, suggestionsLimit)
+	});
+})();
+`````
+
+
+### Example 4
+
+In this case a custom suggestions template is used.
+The template uses `%placeholder%` variables, which come from the extra fields items in the data set have.
+
+`````javascript
+(function () {
+	var dataSet = [
+		{"id": "github", "users": "1+ million", "domain": "github.com"},
+		{"id": "gmail", "users": "400+ million", "domain": "gmail.com"},
+		{"id": "facebook", "users": "1+ billion", "domain": "facebook.com"},
+		{"id": "twitter", "users": "200+ million", "domain": "twitter.com"}
+	];
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplateHtml": "<img src='https://www.google.com/s2/favicons?domain=%domain%' /> <strong>%id%</strong> (%users%)",
+		"dataSource": new Suggestor.LocalDataSource(dataSet)
+	});
+})();
+`````
+
+
+### Example 5
+
+Similar to the example above, but uses a function to dynamically build the HTML for each suggestion entry.
+This allows for more flexibility than mere `%placeholder%` substitution.
+
+`````javascript
+(function () {
+	var dataSet = [
+		{"id": "github", "users": "1+ million", "domain": "github.com"},
+		{"id": "gmail", "users": "400+ million", "domain": "gmail.com"},
+		{"id": "facebook", "users": "1+ billion", "domain": "facebook.com"},
+		{"id": "twitter", "users": "200+ million", "domain": "twitter.com"}
+	];
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplateHtml": function (item) {
+			var imageUrl = 'https://www.google.com/s2/favicons?domain=' + item.domain;
+			return "<img src='" + imageUrl + "' /> <strong>" + item.id + "</strong> (" + item.users + ")";
+		},
+		"dataSource": new Suggestor.LocalDataSource(dataSet)
+	});
+})();
+`````
+
+
+### Example 6
+
+User mentions/suggestions triggered by @query (the start delimiter is "@").
+
+The predefined `users` suggestions template is used for rendering the suggestions list.
+The template requires each entry to have the following additional data fields: `image`, `name`.
+
+`````javascript
+(function () {
+	var dataSet = [
+		{
+			"id": "john.doe",
+			"keywords": ["John", "Doe"],
+			"name": "John Doe",
+			"image": "/path/to/photo-1.jpg"
+		},
+		{
+			"id": "john.brown",
+			"keywords": ["John", "Brown"],
+			"name": "John Brown",
+			"image": "/path/to/photo-2.jpg"
+		},
+		{
+			"id": "jack.brown",
+			"keywords": ["Jack", "Brown"],
+			"name": "Jack Brown",
+			"image": "/path/to/photo-3.jpg"
+		}
+	];
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "@",
+		"suggestionsTemplate": "users",
+		"dataSource": new Suggestor.LocalDataSource(dataSet)
+	});
+})();
+`````
+
+
+### Example 7
+
+Lazily-loaded data set from a URL, instead of a local/hardcoded data set.
+This is only about loading a complete static data-set lazily, not for doing per-query loading from remote. Check the other example for that.
+
+`````javascript
+(function () {
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplate": "tags",
+		"dataSource": new Suggestor.LazilyLoadedDataSource(function (callback) {
+			$.getJSON("/path/to/data-set-tags.json", function (response) {
+				callback(response.tags);
+			});
+		})
+	});
+})();
+`````
+
+Supposing that `/path/to/data-set-tags.json` returns this response:
+
+`````javascript
+{
+	"tags": [
+		{"id": "github"},
+		{"id": "gmail"},
+		{"id": "facebook"},
+		{"id": "twitter"}
+	]
+}
+`````
+
+
+### Example 8
+
+Multiple instances for the same text field.
+
+`````javascript
+(function () {
+	var dataSetTags = [
+		{"id": "github"},
+		{"id": "gmail"},
+		{"id": "facebook"},
+		{"id": "twitter"}
+	];
+
+	var dataSetUsernames = [
+		{"id": "john.doe"},
+		{"id": "john.brown"},
+		{"id": "jack.brown"}
+	];
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplate": "tags",
+		"dataSource": new Suggestor.LocalDataSource(dataSetTags)
+	}).suggestor({
+		"startDelimiter": "@", //this is the default value anyway
+		"suggestionsTemplate": "basic", //this is the default value anyway
+		"dataSource": new Suggestor.LocalDataSource(dataSetUsernames)
+	});
+})();
+`````
+
+
+### Example 9
+
+Per-query data source.
+No local matching is performed by default - the server is supposed to return only entries that match.
+The results for each query are cached and reused automatically.
+
+`````javascript
+(function () {
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplate": "tags",
+		"dataSource": new Suggestor.PerQueryDataSource(function (query, callback) {
+			var uri = "/some/path/_suggest?query=" + query;
+
+			$.getJSON(uri, function (response) {
+				callback(response.tags);
+			});
+		}
+	});
+})();
+`````
+
+Supposing that `/some/path/_suggest?query=g` returns a similar response:
+
+`````javascript
+{
+	"tags": [
+		{"id": "github"},
+		{"id": "gmail"}
+	]
+}
+`````
+
+
+### Example 10
+
+Advanced example that overrides the default suggestions utilization logic.
+
+`````javascript
+(function () {
+	//Start from the default Suggestor.SuggestionsUtilizer class
+	//and override it a little bit.
+	var MyUtilizer = function () {};
+	MyUtilizer.prototype = new Suggestor.SuggestionsUtilizer();
+	MyUtilizer.prototype.utilize = function (item) {
+		alert("You chose this suggestion: " + item.id);
+		this.context.$textField.val(''); //let's clear the text field
+	};
+
+	var dataSet = [
+		{"id": "github"},
+		{"id": "gmail"},
+		{"id": "facebook"},
+		{"id": "twitter"}
+	];
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplate": "tags",
+		"suggestionsUtilizer": new MyUtilizer(),
+		"dataSource": new Suggestor.LocalDataSource(dataSet)
+	});
+})();
+`````
+
+
+### Example 11
+
+Advanced example that overrides the default matching logic.
+
+`````javascript
+(function () {
+	//Start from the default Suggestor.Matcher class and override it a little bit.
+	var MyMatcher = function () {};
+	MyMatcher.prototype = new Suggestor.Matcher();
+	MyMatcher.prototype.matches = function (item, query) {
+		//Case-sensitive "prefix" match. For longer queries only. Against the id field only.
+		return (query.length >= 3 && (item.id.substring(0, query.length) === query));
+	};
+
+	var dataSet = [
+		{"id": "github"},
+		{"id": "gmail"},
+		{"id": "facebook"},
+		{"id": "twitter"}
+	];
+
+	var suggestionsLimit = null; //no limit
+
+	$('#js-textarea-suggestor').suggestor({
+		"startDelimiter": "#",
+		"suggestionsTemplate": "tags",
+		"dataSource": new Suggestor.LocalDataSource(dataSet, suggestionsLimit, new MyMatcher())
+	});
+})();
+`````
+
+
+Doing more
+----------
+
+**suggestor.js** is highly customizable.
+
+Various parts can be replaced with your own custom version. Such are:
+ * the QueryExtractor - inspects the text field and determines which part of it is the query
+ * the DataSource - takes care of providing suggestions data (locally or remotely)
+ * the Matcher (often used with a DataSource) - narrows down the suggestions list to only those items that match (may not be used if matching is done on the server-side)
+ * the SuggestionsRenderer - renders the suggestions list/picker
+ * the SuggestionsUtilizer - utilizes the selected suggestion value (changes the text field contents)
+
+Look at the source code for more information.
+
+
+Areas for improvement
+---------------------
+
+ * Empty querying - being able to enter "@" to get the full list of suggestions
+ * Empty delimiter querying - getting suggestions for any word without having to enter a delimiter
+ * Automatic HTML-escaping of `%placeholder%` variables in templates
+ * More examples and documentation
---
+++ suggestor.js
@@ -0,0 +1,459 @@
+/** suggestor.js 1.0 - BSD licensed - https://github.com/spantaleev/suggestor.js **/
+
+(function ($) {
+	window.Suggestor = {};
+
+	var preg_quote = function (str, delimiter) {
+		//Source: http://phpjs.org/functions/preg_quote/
+		// +   original by: booeyOH
+		// +   improved by: Ates Goral (http://magnetiq.com)
+		// +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// +   bugfixed by: Onno Marsman
+		// +   improved by: Brett Zamir (http://brett-zamir.me)
+		return (str + '').replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
+	};
+
+	var KEY_CODE = {"TAB": 9, "ENTER": 13, "ESCAPE": 27, "ARROW_UP": 38, "ARROW_DOWN": 40};
+
+	Suggestor.TextFieldHelper = function () {};
+	Suggestor.TextFieldHelper.STOP_CHARS_LIST = [" ", ",", "?", "!"];
+	Suggestor.TextFieldHelper.prototype = {
+		getState: function ($textField, startDelimiter) {
+			if (startDelimiter.length !== 1) {
+				throw new Error("Only single-character delimiters are supported, not: " + startDelimiter);
+			}
+
+			var cursorPosition = $textField[0].selectionStart,
+				queryStartPosition,
+				queryEndPosition = cursorPosition,
+				text = $textField.val(),
+				startDelimiterFound = false,
+				query = null;
+
+			for (queryStartPosition = cursorPosition - 1; queryStartPosition >= 0; --queryStartPosition) {
+				if (Suggestor.TextFieldHelper.STOP_CHARS_LIST.indexOf(text[queryStartPosition]) !== -1) {
+					break;
+				}
+
+				if (text[queryStartPosition] === startDelimiter) {
+					startDelimiterFound = true;
+					break;
+				}
+			}
+
+			if (startDelimiterFound) {
+				query = text.substring(queryStartPosition + 1);
+				query = query.replace(/^(.+?)([\s,!?])(.|\n)*/m, "$1");
+			}
+
+			return {
+				"text": text,
+				"cursorPosition": cursorPosition,
+				"queryStartPosition": queryStartPosition,
+				"queryEndPosition": queryEndPosition,
+				"query": query
+			};
+		}
+	};
+
+	Suggestor.QueryExtractor = function (textFieldHelper) {
+		this.textFieldHelper = textFieldHelper;
+		this.context = null;
+	};
+	Suggestor.QueryExtractor.prototype = {
+		initialize: function (context) {
+			this.context = context;
+		},
+
+		getQuery: function () {
+			return this.textFieldHelper.getState(this.context.$textField, this.context.settings.startDelimiter).query;
+		}
+	};
+
+	Suggestor.SuggestionsUtilizer = function (textFieldHelper) {
+		this.textFieldHelper = textFieldHelper;
+		this.context = null;
+	};
+	Suggestor.SuggestionsUtilizer.prototype = {
+		initialize: function (context) {
+			this.context = context;
+		},
+
+		utilize: function (suggestionItem) {
+			var $textField = this.context.$textField,
+				state = this.textFieldHelper.getState($textField, this.context.settings.startDelimiter),
+				textBefore = state.text.substring(0, state.queryStartPosition),
+				textAfter = state.text.substring(state.queryEndPosition),
+				suggestionValue = (this.context.settings.startDelimiter + suggestionItem.id);
+
+			this.context.$textField.val(textBefore + suggestionValue + textAfter);
+			$textField[0].selectionStart = $textField[0].selectionEnd = (textBefore + suggestionValue).length;
+
+			//Restore focus to the text field, if it got lost (due to a suggestions list click)
+			$textField.focus();
+		}
+	};
+
+	Suggestor.SuggestionsRenderer = function () {};
+	Suggestor.SuggestionsRenderer.TEMPLATES = {
+		"basic": "%id%",
+		"tags": "#%id%",
+		"users": "<img src='%image%' class='suggestor-image' /><strong>%name%</strong> <span class='suggestor-picker-tooltip'>@%id%</span>"
+	};
+	Suggestor.SuggestionsRenderer.prototype = {
+		initialize: function (context) {
+			var settings = context.settings;
+
+			if (settings.suggestionsTemplateHtml === null) {
+				settings.suggestionsTemplateHtml = Suggestor.SuggestionsRenderer.TEMPLATES[settings.suggestionsTemplate];
+			}
+
+			this.template = settings.suggestionsTemplateHtml; //string or function
+			this.context = context;
+			this.items = null;
+			this.currentIndex = 0;
+			this.openStatus = false;
+			this.$container = $($.parseHTML('<div class="suggestor-container"></div>'));
+			this.$container.insertAfter(context.$textField);
+
+			this._setupEventHandlers();
+		},
+
+		isOpen: function () {
+			return this.openStatus;
+		},
+
+		set: function (items) {
+			this.items = items;
+			this.currentIndex = 0;
+		},
+
+		open: function () {
+			var self = this;
+
+			var createHtml = function (items, selectedIndex) {
+				var html = '';
+				html += '<ul class="js-suggestor-picker suggestor-picker">';
+				$.each(items, function (idx, item) {
+					var entryClass = 'js-suggestor-suggestion js-suggestor-suggestion-' + idx + ' suggestor-suggestion';
+					if (idx === selectedIndex) {
+						entryClass += ' suggestor-suggestion-active';
+					}
+					html += '<li class="' + entryClass + '" data-index="' + idx + '"><a>' + self._renderItem(item) + '</a></li>';
+				});
+				html += '</ul>';
+				return html;
+			};
+
+			this.openStatus = true;
+			this.$container.html(createHtml(this.items, this.currentIndex));
+
+			this.$container.css({
+				"top": this.context.$textField.offset().top + this.context.$textField.height(),
+				"left": this.context.$textField.offset().left
+			});
+		},
+
+		close: function () {
+			this.currentIndex = 0;
+			this.openStatus = false;
+			this.$container.find('.js-suggestor-picker').remove();
+		},
+
+		_moveUp: function () {
+			this.currentIndex = (this.currentIndex > 0 ? this.currentIndex - 1 : this.items.length - 1);
+			this._updateActive();
+			this._scrollActiveIntoView();
+		},
+
+		_moveDown: function () {
+			this.currentIndex = (this.currentIndex >= this.items.length - 1 ? 0 : this.currentIndex + 1);
+			this._updateActive();
+			this._scrollActiveIntoView();
+		},
+
+		_getCurrentItem: function () {
+			if (this.items === null) {
+				return null;
+			}
+			return (this.items[this.currentIndex] ? this.items[this.currentIndex] : null);
+		},
+
+		_selectCurrentItem: function () {
+			this.context.onItemSelected(this._getCurrentItem());
+			this.close();
+		},
+
+		_setupEventHandlers: function () {
+			var self = this;
+
+			this.context.$textField.on('keydown', function (ev) {
+				if (!self.isOpen()) {
+					return;
+				}
+
+				if (ev.keyCode === KEY_CODE.ARROW_UP) {
+					ev.preventDefault();
+					self._moveUp();
+				} else if (ev.keyCode === KEY_CODE.ARROW_DOWN) {
+					ev.preventDefault();
+					self._moveDown();
+				} else if (ev.keyCode === KEY_CODE.ESCAPE) {
+					ev.preventDefault();
+					self.close();
+				} else if (ev.keyCode === KEY_CODE.ENTER || ev.keyCode === KEY_CODE.TAB) {
+					ev.preventDefault();
+					self._selectCurrentItem();
+				}
+			});
+
+			this.$container.on('mouseover', '.js-suggestor-suggestion', function () {
+				self.currentIndex = $(this).data('index');
+				self._updateActive();
+			});
+
+			this.$container.on('click', '.js-suggestor-suggestion', function (ev) {
+				ev.preventDefault();
+				self._selectCurrentItem();
+			});
+		},
+
+		_renderItem: function (item) {
+			if (typeof(this.template) === 'function') {
+				return this.template(item);
+			}
+
+			var html = this.template,
+				regexVariables = /%(.+?)%/;
+			while (match = regexVariables.exec(html)) {
+				html = html.replace(match[0], item[match[1]]);
+			}
+			return html;
+		},
+
+		_updateActive: function () {
+			this.$container.find('.suggestor-suggestion-active').removeClass('js-suggestor-suggestion-active suggestor-suggestion-active');
+			this.$container.find('.js-suggestor-suggestion-' + this.currentIndex).addClass('js-suggestor-suggestion-active suggestor-suggestion-active');
+		},
+
+		_scrollActiveIntoView: function () {
+			var $item = this.$container.find('.js-suggestor-suggestion-active'),
+				$list = $item.closest('ul'),
+				listMaxHeight = parseInt($list.css('maxHeight'), 10);
+
+			if (listMaxHeight) {
+				//Item may be below/above the viewport, respectively. Scroll it into view.
+				if (($item.offset().top > $list.offset().top + listMaxHeight) || ($item.offset().top < $list.offset().top)) {
+					$list.scrollTop($item.offset().top - $list.offset().top);
+				}
+			}
+		}
+	};
+
+	Suggestor.Matcher = function () {};
+	Suggestor.Matcher.prototype = {
+		matches: function (item, query) {
+			if (typeof(item.keywords) === 'undefined') {
+				item.keywords = [];
+			}
+
+			//Always match against the id field.
+			item.keywords.push(item.id);
+
+			var isMatch = false;
+			$.each(item.keywords, function (_idx, keyword) {
+				if ((new RegExp("^" + preg_quote(query), "i")).test(keyword)) {
+					isMatch = true;
+					return false;
+				}
+			});
+			return isMatch;
+		}
+	};
+
+	Suggestor.LocalDataSource = function (items, limit, matcher) {
+		this.items = items;
+		this.limit = limit;
+		this.matcher = (typeof(matcher) === 'undefined' ? new Suggestor.Matcher() : matcher);
+	};
+	Suggestor.LocalDataSource.prototype = {
+		suggest: function (query, callback) {
+			var self = this,
+				itemsCount = 0;
+
+			var items = $.grep(this.items, function (item) {
+				if (self.limit && itemsCount >= self.limit) {
+					return false;
+				}
+
+				if (self.matcher === null || self.matcher.matches(item, query)) {
+					itemsCount += 1;
+					return true;
+				}
+
+				return false;
+			});
+
+			callback(items);
+		}
+	};
+
+	Suggestor.LazilyLoadedDataSource = function (dataLoader, matcher) {
+		this.dataLoader = dataLoader;
+		this.matcher = (typeof(matcher) === 'undefined' ? new Suggestor.Matcher() : matcher);
+		this.localDataSource = null;
+		this.isLoading = false;
+		this.onDataSourceLoaded = function () {};
+	};
+	Suggestor.LazilyLoadedDataSource.prototype = {
+		suggest: function (query, callback) {
+			if (this.localDataSource === null) {
+				var self = this;
+
+				//Overwrite this for each new query we get while loading, so that when the data source loads,
+				//we'll provide suggestions for the last query that we got, not for the first one.
+				this.onDataSourceLoaded = function () {
+					self.localDataSource.suggest(query, callback);
+				};
+
+				if (!this.isLoading) {
+					this.isLoading = true;
+					this.dataLoader(function (items) {
+						self.localDataSource = new Suggestor.LocalDataSource(items, self.matcher);
+						self.onDataSourceLoaded();
+					});
+				}
+			} else {
+				this.localDataSource.suggest(query, callback);
+			}
+		}
+	};
+
+	Suggestor.PerQueryDataSource = function (dataLoader, matcher) {
+		this.dataLoader = dataLoader;
+		this.matcher = (typeof(matcher) === 'undefined' ? null : matcher);
+		this.cache = {};
+		this.lastQuery = null;
+	};
+	Suggestor.PerQueryDataSource.prototype = {
+		suggest: function (query, callback) {
+			this.lastQuery = query;
+
+			if (query in this.cache) {
+				this._suggest(query, this.cache[query], callback);
+			} else {
+				var self = this;
+				this.dataLoader.call(this, query, function (items) {
+					self.cache[query] = items;
+					self._suggest(query, self.cache[query], callback);
+				});
+			}
+		},
+
+		_suggest: function (query, items, callback) {
+			//If the query changed, ignore this "out-of-order" response.
+			if (this.lastQuery === query) {
+				var ds = new Suggestor.LocalDataSource(items, this.matcher);
+				ds.suggest(query, callback);
+			}
+		}
+	};
+
+	var validateSettings = function (settings) {
+		if (!settings.dataSource) {
+			throw new Error("At least the dataSource parameter needs to be specified.");
+		}
+	};
+
+	var validateItems = function (items) {
+		$.each(items, function (_idx, item) {
+			if (typeof(item.id) === 'undefined') {
+				throw new Error("Items need to have an id field at least.");
+			}
+		});
+	};
+
+	var setupEventHandlers = function ($textField, settings) {
+		var lastQuery = null;
+		$textField.on('keyup change', function (ev) {
+			var query = settings.queryExtractor.getQuery();
+
+			if (!query) {
+				lastQuery = null;
+				settings.suggestionsRenderer.close();
+				return;
+			}
+
+			if (query !== lastQuery) {
+				lastQuery = query;
+				settings.dataSource.suggest(query, function (items) {
+					validateItems(items);
+
+					if (items.length === 0) {
+						settings.suggestionsRenderer.close();
+					} else if (items.length === 1 && items[0].id === query) {
+						settings.suggestionsRenderer.close();
+					} else {
+						settings.suggestionsRenderer.set(items);
+						settings.suggestionsRenderer.open();
+					}
+				});
+			}
+		});
+
+		$textField.on('blur', function () {
+			//This text field blur event may be caused by a click on an item in the suggestions container.
+			//Delay closing the suggestions window a bit, so that such a click event would get through.
+			window.setTimeout(function () {
+				settings.suggestionsRenderer.close();
+			}, 1000);
+		});
+	};
+
+	var registerTextField = function ($textField, options) {
+		var settings = $.extend({}, {
+			//The (single character) delimiter that identifies where a suggestion starts
+			"startDelimiter": "@",
+
+			//A textarea/input query extractor - see Suggestor.QueryExtractor
+			"queryExtractor": new Suggestor.QueryExtractor(new Suggestor.TextFieldHelper()),
+
+			//Template to use, from the predefined in Suggestor.SuggestionsRenderer.TEMPLATES
+			"suggestionsTemplate": "basic",
+
+			//Template HTML, in case `suggestionsTemplate` is not enough and you want a custom one
+			"suggestionsTemplateHtml": null,
+
+			//A renderer/picker object - see Suggestor.SuggestionsRenderer
+			"suggestionsRenderer": new Suggestor.SuggestionsRenderer(),
+
+			//A textarea/input suggestion utilizer - see Suggestor.SuggestionsUtilizer
+			"suggestionsUtilizer": new Suggestor.SuggestionsUtilizer(new Suggestor.TextFieldHelper())
+		}, options);
+
+		validateSettings(settings);
+
+		var context = {
+			$textField: $textField,
+			settings: settings,
+			onItemSelected: function (item) {
+				if (item !== null) {
+					settings.suggestionsUtilizer.utilize(item);
+				}
+			}
+		};
+
+		settings.queryExtractor.initialize(context);
+		settings.suggestionsRenderer.initialize(context);
+		settings.suggestionsUtilizer.initialize(context);
+
+		setupEventHandlers($textField, settings);
+	};
+
+	$.fn.suggestor = function (options) {
+		$(this).each(function () {
+			registerTextField($(this), options);
+		});
+		return this;
+	};
+})(jQuery);
